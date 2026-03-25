e6ca3de676f8a3d6749d1de58d56d58adfd8bfe1
ypenevp <ypenevp@gmail.com>
2026-03-26 01:41:32 +0200

    setup: frontend

---
+++ frontend/app/App.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import "./global.css"
+import { DATA } from "@env";
+
+export default function App() {
+  return (
+    <View className="items-center justify-center mt-[100%]">
+      <Text className="font-bold text-green-500">Welcome to Legendss Team!</Text> 
+      <Text className="font-bold text-green-500">{DATA}</Text>  
+    </View>
+  );
+}
